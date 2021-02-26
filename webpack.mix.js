c4fd4a041b0cdd113600ba6bf641fee320a0c698
Florian Bouvot <florian.bouvot@gmail.com>
2021-02-26 21:07:52 +0100

    Use laravel-mix-copy-watched extension + rename images folders

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ const mix = require('laravel-mix');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+require('laravel-mix-copy-watched');
 require('laravel-mix-nunjucks')
 
 mix
@@ -10,67 +11,66 @@ mix
   .sass('src/css/app.scss', 'css').options({
     processCssUrls: false
   })
+  .copyWatched('src/fonts/**/*.{woff,woff2}', 'dist/fonts')
+  .webpackConfig({
+    plugins: [
+      new CleanWebpackPlugin(),
+    ]
+  })
+  .njk('src/templates/', 'dist/')
   .browserSync({
     server: 'dist/',
     files: [
       'src/templates/**/*.html',
       'tailwind.config.js',
     ],
-  });
-
-mix.copy('src/fonts/**/*.{woff,woff2}', 'dist/fonts');
-
-mix.njk('src/templates/', 'dist/');
-
-mix.webpackConfig({
-  plugins: [
-    new CleanWebpackPlugin(),
-  ]
-});
+  })
 
 if (mix.inProduction()) {
-  mix.version();
-
-  mix.webpackConfig({
-    module: {
-      rules: [
-        {
-          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-          type: 'asset/resource',
-        },
-      ],
-    },
-    plugins: [
-      new CopyPlugin({
-        patterns: [
+  mix
+    .version()
+    .webpackConfig({
+      module: {
+        rules: [
           {
-            from: '**/*.{gif,jpg,png,svg,ico}',
-            to: 'img',
-            context: 'src/img',
+            test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+            type: 'asset/resource',
           },
         ],
-      }),
-      new ImageMinimizerPlugin({
-        minimizerOptions: {
-          plugins: [
-            ['gifsicle'],
-            ['mozjpeg', { quality: 50 }],
-            ['pngquant', { quality: [0.5, 0.5] }],
-            [
-              'svgo',
-              {
-                plugins: [
-                  {
-                    removeViewBox: false,
-                  },
-                ],
-              },
-            ],
+      },
+      plugins: [
+        new CopyPlugin({
+          patterns: [
+            {
+              from: '**/*.{gif,jpg,png,svg,ico}',
+              to: 'images',
+              context: 'src/images',
+            },
           ],
-        },
-      }),
-    ],
-  });
-} else {
-  mix.copy('src/img/**/*.{gif,jpg,png,svg,ico}', 'dist/img');
+        }),
+        new ImageMinimizerPlugin({
+          minimizerOptions: {
+            plugins: [
+              ['gifsicle'],
+              ['mozjpeg', { quality: 50 }],
+              ['pngquant', { quality: [0.5, 0.5] }],
+              [
+                'svgo',
+                {
+                  plugins: [
+                    {
+                      removeViewBox: false,
+                    },
+                  ],
+                },
+              ],
+            ],
+          },
+        }),
+      ],
+    })
+}
+else {
+  mix.
+    copyWatched('src/images/**/*.{gif,jpg,png,svg,ico}', 'dist/images')
 }
